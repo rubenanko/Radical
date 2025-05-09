ae0be4a5d05af6f1e64dcd77a103baac56194e3b
rubenanko <rubenanko@gmail.com>
2025-05-09 12:40:38 +0200

    undo: state persistence

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,41 +1,33 @@
+var state = false;
 
 function removeTab(tab) {chrome.tabs.remove(tab.id)}
 
 function removeWindow(window) {chrome.windows.remove(window.id)}
 
-chrome.storage.session.get(["state"]).then((result) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        // sendResponse({message: "Service worker processed the message"})
 
-    var state = result.state;
-    console.log(state);
-
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-            // sendResponse({message: "Service worker processed the message"})
-
-            if(request["action"] == "toggle")
-            {
-                state = ! state;
-                console.log(state);
-                chrome.storage.session.set({"state" : state});
-
-                if(state)
-                    {
-                        chrome.tabs.onCreated.addListener(removeTab);
-                        chrome.windows.onCreated.addListener(removeWindow);
-                        chrome.action.setIcon({ path: "../images/enabled_icon_128.png"});
-                    }
-                else
+        if(request["action"] == "toggle")
+        {
+            state = ! state;
+            if(state)
                 {
-                    chrome.tabs.onCreated.removeListener(removeTab);
-                    chrome.windows.onCreated.removeListener(removeWindow);
-                    chrome.action.setIcon({ path: "../images/disabled_icon_128.png"});
+                    chrome.tabs.onCreated.addListener(removeTab);
+                    chrome.windows.onCreated.addListener(removeWindow);
+                    chrome.action.setIcon({ path: "../images/enabled_icon_128.png"});
                 }
-            }
-
-            if(request["action"] == "get-state")
+            else
             {
-                sendResponse({"state":state});
+                chrome.tabs.onCreated.removeListener(removeTab);
+                chrome.windows.onCreated.removeListener(removeWindow);
+                chrome.action.setIcon({ path: "../images/disabled_icon_128.png"});
             }
         }
-    )
 
-});
+        if(request["action"] == "get-state")
+        {
+            sendResponse({"state":state});
+        }
+    }
+  )
+  
